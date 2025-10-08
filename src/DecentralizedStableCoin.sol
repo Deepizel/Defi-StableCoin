58625783fed0ff58170ab89572dfd615b9f7e7a2
Victor Ekundayo <rocmancave@gmail.com>
2025-10-08 19:28:12 +0100

    fixed cl fails with forge fmt and updated base logic

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 /*
 * @title Decentralized Stable Coin
 * Stability Mechanism: Algorithmic (Decentralization)
@@ -12,11 +13,31 @@ import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensio
 
 */
 
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__NotZeroAddress();
 
-contract DecentralizedStableCoin is ERC20Burnable {
-    constructor() ERC20("Decentralized Stable Coin", "DSC") {
+    constructor(address initialOwner) ERC20("Decentralized Stable Coin", "DSC") Ownable(initialOwner) {}
+
+    function burn(uint256 amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (amount > balance) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        if (amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        super.burn(amount);
     }
 
-
-
+    function mint(address _to, uint256 amount) public onlyOwner {
+        if (amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        _mint(_to, amount);
+    }
 }
